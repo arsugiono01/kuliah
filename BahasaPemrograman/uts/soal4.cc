d69b5e97f2248495dcb4ab329eed48d9430eb0bc
root <root@DESKTOP-6EVE6MO.localdomain>
2021-11-17 20:50:08 +0700

    uts

---
+++ BahasaPemrograman/uts/soal4.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int tahunlahir = 2000;
+    int tahunsekarang = 2021;
+    int usia = tahunsekarang - tahunlahir; 
+
+    cout<<"Berarti Usia Kalian Sekarang adalah "<< usia << " Tahun"<<endl;
+    return 0; 
+}
