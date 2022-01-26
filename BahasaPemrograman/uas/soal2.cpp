b9fcafc3f4434fb1a8ff7942b7cc6fc000d3bd84
root <root@DESKTOP-6EVE6MO.localdomain>
2022-01-26 22:30:06 +0700

    UAS

---
+++ BahasaPemrograman/uas/soal2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void nama()
+{
+    string nama;
+
+    cout << "Masukan Nama : ";
+    getline(cin, nama);
+
+    
+}
+
+void nim()
+{
+    string nim;
+
+    cout << "Masukan NIM : ";
+    cin>>nim;
+
+    
+}
+
+int main()
+{
+    nama() ;
+    nim() ;
+
+    return 0;
+}
